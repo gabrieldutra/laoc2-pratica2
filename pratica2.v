9e335d3f4c0f71dca27c4f7bf8ccbc602aa387eb
Gabriel Dutra <nesk.frz@gmail.com>
2018-04-27 16:47:25 -0300

    Instruções mv e mvi

--- pratica2.v
+++ pratica2.v
@@ -1,8 +1,56 @@
-module pratica2 (Clock, Done);
-	input Clock;
-	output Done;
-	wire [15:0] BusWires;
+module pratica2 (SW, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7, LEDR);
+	input [17:0] SW;
+	output [0:6] HEX0;
+	output [0:6] HEX1;
+	output [0:6] HEX2;
+	output [0:6] HEX3;
+	output [0:6] HEX4;
+	output [0:6] HEX5;
+	output [0:6] HEX6;
+	output [0:6] HEX7;
+	output [2:0] LEDR;
 	
-	proc process (16'b0000000000000000, 1'b0, Clock, 1'b1, Done, BusWires);
+	// Displays
+	wire [0:6] display1;
+	wire [0:6] display2;
+	wire [0:6] display3;
+	wire [0:6] display4;
+	wire [0:6] display5;
+	wire [0:6] display6;
+	wire [0:6] display7;
+	wire [0:6] display8;
+	
+	// wires
+	wire Done;
+	wire Clock;
+	wire [15:0] BusWires;
+	wire [15:0] DIN;
+	wire [1:0] State;
+	
+	// Assign for displays
+	assign HEX0 = display1;
+	assign HEX1 = display2;
+	assign HEX2 = display3;
+	assign HEX3 = display4;
+	assign HEX4 = display5;
+	assign HEX5 = display6;
+	assign HEX6 = display7;
+	assign HEX7 = display8;
+	
+	assign LEDR[0:0] = Done;	
+	assign Clock = SW[17];
+	assign DIN = SW[15:0];
+	assign LEDR[2:1] = State[1:0];
+	
+	proc process (DIN, 1'b0, Clock, 1'b1, Done, BusWires, State);
+	bcd_7seg bcd1 (BusWires[3:0], display1[0:6]);
+	bcd_7seg bcd2 (BusWires[7:4], display2[0:6]);
+	bcd_7seg bcd3 (BusWires[11:8], display3[0:6]);
+	bcd_7seg bcd4 (BusWires[15:12], display4[0:6]);
+	
+	bcd_7seg bcd5 (DIN[3:0], display5[0:6]);
+	bcd_7seg bcd6 (DIN[7:4], display6[0:6]);
+	bcd_7seg bcd7 (DIN[11:8], display7[0:6]);
+	bcd_7seg bcd8 (DIN[15:12], display8[0:6]);
 
 endmodule
--- proc.v
+++ proc.v
@@ -1,13 +1,20 @@
-module proc (DIN, Resetn, Clock, Run, Done, BusWires);
+module proc (DIN, Resetn, Clock, Run, Done, BusWires, State);
 	input [15:0] DIN;
 	input Resetn, Clock, Run;
-	output reg Done;
+	output reg Done = 0;
 	output [15:0] BusWires;
+	output [1:0] State;
 	
 	wire [15:0] IR;
 	wire [15:0] R [0:7];
 	wire RX, RY;
 	wire Xreg, Yreg;
+	wire [1:0] Tstep_Q;
+	
+	// regs
+	wire [15:0] G;
+	wire [15:0] A;
+	wire [15:0] GinData;
 	
 	regn regI (DIN, IRin, Clock, IR);
 	
@@ -20,10 +27,13 @@ module proc (DIN, Resetn, Clock, Run, Done, BusWires);
 	reg Rout[0:7];
 	reg Ain;
 	reg Gin;
+	reg Aout;
+	reg Gout;
 	reg DINout;
+	reg sub = 0;
 	
 	wire Clear = Resetn;
-	upcount Tstep (Clear, Clock, Tstep_Q);
+	upcount Tstep (1'b0, Clock, Tstep_Q);
 	dec3to8 decX (RX, 1'b1, Xreg);
 	dec3to8 decY (RY, 1'b1, Yreg);
 	
@@ -33,9 +43,50 @@ module proc (DIN, Resetn, Clock, Run, Done, BusWires);
 			2'b00: // time step 0 - store DIN in IR on step 0
 			begin
 				IRin = 1'b1; // 
+				Done = 0;
+				Rin[0] = 0;
+				Rin[1] = 0;
+				Rin[2] = 0;
+				Rin[3] = 0;
+				Rin[4] = 0;
+				Rin[5] = 0;
+				Rin[6] = 0;
+				Rin[7] = 0;
+				Ain = 0;
+				Gin = 0;
+				Rout[0] = 0;
+				Rout[1] = 0;
+				Rout[2] = 0;
+				Rout[3] = 0;
+				Rout[4] = 0;
+				Rout[5] = 0;
+				Rout[6] = 0;
+				Rout[7] = 0;
+				DINout = 0;
 			end		
 			2'b01: // time step 1 - (mv: RYout, RXin and Done; mvi: DINout and RXin; add: RXout, Ain; sub: RXout, Ain)
 			begin
+				IRin = 0;
+				Done = 0;
+				Rin[0] = 0;
+				Rin[1] = 0;
+				Rin[2] = 0;
+				Rin[3] = 0;
+				Rin[4] = 0;
+				Rin[5] = 0;
+				Rin[6] = 0;
+				Rin[7] = 0;
+				Ain = 0;
+				Gin = 0;
+				Rout[0] = 0;
+				Rout[1] = 0;
+				Rout[2] = 0;
+				Rout[3] = 0;
+				Rout[4] = 0;
+				Rout[5] = 0;
+				Rout[6] = 0;
+				Rout[7] = 0;
+				DINout = 0;
 				case(I)
 					3'b000: // mv: RYout, RXin and Done
 					begin
@@ -51,23 +102,65 @@ module proc (DIN, Resetn, Clock, Run, Done, BusWires);
 					end
 					3'b010: // add: RXout, Ain
 					begin
-						Rout[RY] = 1;
-						Rin[RX] = 1;
-						Done = 1;
+						Rout[RX] = 1;
+						Ain = 1;
+						Done = 0;
+						sub = 0;
 					end
 					3'b011: // sub: RXout, Ain
 					begin
-						Rout[RY] = 1;
-						Rin[RX] = 1;
-						Done = 1;
+						Rout[RX] = 1;
+						Ain = 1;
+						Done = 0;
+						sub = 1;
 					end
 				endcase
 			end		
 			2'b10: // time step 2
-			begin
+			begin				
+				Done = 0;
+				Rin[0] = 0;
+				Rin[1] = 0;
+				Rin[2] = 0;
+				Rin[3] = 0;
+				Rin[4] = 0;
+				Rin[5] = 0;
+				Rin[6] = 0;
+				Rin[7] = 0;
+				Ain = 0;
+				Gin = 0;
+				Rout[0] = 0;
+				Rout[1] = 0;
+				Rout[2] = 0;
+				Rout[3] = 0;
+				Rout[4] = 0;
+				Rout[5] = 0;
+				Rout[6] = 0;
+				Rout[7] = 0;
+				DINout = 0;
 			end		
 			2'b11: // time step 3
-			begin	
+			begin				
+				Done = 0;
+				Rin[0] = 0;
+				Rin[1] = 0;
+				Rin[2] = 0;
+				Rin[3] = 0;
+				Rin[4] = 0;
+				Rin[5] = 0;
+				Rin[6] = 0;
+				Rin[7] = 0;
+				Ain = 0;
+				Gin = 0;
+				Rout[0] = 0;
+				Rout[1] = 0;
+				Rout[2] = 0;
+				Rout[3] = 0;
+				Rout[4] = 0;
+				Rout[5] = 0;
+				Rout[6] = 0;
+				Rout[7] = 0;
+				DINout = 0;
 			end
 		endcase
 	end
@@ -81,7 +174,14 @@ module proc (DIN, Resetn, Clock, Run, Done, BusWires);
 	regn reg5 (BusWires, Rin[5], Clock, R[5]);
 	regn reg6 (BusWires, Rin[6], Clock, R[6]);
 	regn reg7 (BusWires, Rin[7], Clock, R[7]);	
+	regn regA (BusWires, Ain, Clock, A);
+	regn regG (GinData, Gin, Clock, G);
 	
-	assign BusWires = ({8{Rout[0]}} & R[0]) | ({8{Rout[1]}} & R[1]) | ({8{Rout[2]}} & R[2]) | ({8{Rout[3]}} & R[3])
-	 | ({8{Rout[4]}} & R[4]) | ({8{Rout[5]}} & R[5]) | ({8{Rout[6]}} & R[6]) | ({8{Rout[7]}} & R[7]);
+	// somador
+	assign GinData = ({16{~sub}} & (A[15:0] + BusWires));
+	
+	assign BusWires = ({16{Rout[0]}} & R[0]) | ({16{Rout[1]}} & R[1]) | ({16{Rout[2]}} & R[2]) | ({16{Rout[3]}} & R[3])
+	 | ({16{Rout[4]}} & R[4]) | ({16{Rout[5]}} & R[5]) | ({16{Rout[6]}} & R[6]) | ({16{Rout[7]}} & R[7]) | ({16{DINout}} & DIN) 
+	 | ({16{Gout}} & G) | ({16{Aout}} & A);
+	assign State = Tstep_Q;
 endmodule
